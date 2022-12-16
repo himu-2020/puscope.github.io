423e681d8fc2ca66d122189c34b57a4229763970
Himanshu Jawale <69677416+himu-2020@users.noreply.github.com>
2022-12-16 22:19:37 +0530

    update js

--- main.js
+++ main.js
@@ -24,7 +24,17 @@ $(document).ready(function () {
         $('.accordion .accordion-header span').text('+');
         $(this).children('span').text('-');
     });
-
+    
+     const readMoreBtn = document.querySelector(".read-more-btn");
+    const text = document.querySelector(".text");
+    readMoreBtn.addEventListener("click", (e) => {
+        text.classList.toggle("show-more");
+        if (readMoreBtn.innerText === "Read More") {
+            readMoreBtn.innerText = "Read Less";
+        } else {
+            readMoreBtn.innerText = "Read More";
+        }
+    });
 
 
 });
